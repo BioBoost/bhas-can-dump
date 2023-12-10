3c4f0ddaa8eee9f92d6e1e09a2dbfdac835cf032
Nico De Witte <n.dewitte@televic.com>
2023-12-10 15:11:10 +0100

    feat: cleanup and remove library - no need for it at the moment

--- src/main.cpp
+++ src/main.cpp
@@ -7,39 +7,42 @@
 
 DigitalOut activity(LED1);
 DigitalOut alive(LED2);
-// CAN canBus(D15, D14);     // RD, TX => NUCLEO
-CAN canBus(p30, p29);        // RD, TX => LPC
-Thread listenerThread;
+
+#if defined(TARGET_LPC1768)
+  CAN canBus(p30, p29);     // RD, TX => LPC
+#elif defined(TARGET_NUCLEO_L476RG)
+  CAN canBus(D15, D14);     // RD, TX => NUCLEO
+#endif
+
 Timer timer;
+std::chrono::microseconds previousTime;
+char buffer[64] = {};
 
-void can_listener() {
+void can_receive() {
   CANMessage msg;
-  char buffer[64] = {};
-  auto previousTime = timer.elapsed_time();
 
-  while(true) {
-    activity = false;
-    if (canBus.read(msg)) {
-      unsigned int offset = 0;
-      for (unsigned int i = 0; i < msg.len; i++) {
-        offset += sprintf(buffer+offset, "0x%02x ", msg.data[i]);
-      }
-
-      auto now = timer.elapsed_time();
-      printf("_%llums_ (%llums since previous) CAN message received: [CANID = %d]: %s\r\n",
-          std::chrono::duration_cast<std::chrono::milliseconds>(now).count(),
-          std::chrono::duration_cast<std::chrono::milliseconds>(now - previousTime).count(),
-          msg.id, buffer
-        );
-      
-      previousTime = now;
-      activity = true;
+  activity = false;
+  if (canBus.read(msg)) {
+    unsigned int offset = 0;
+    for (unsigned int i = 0; i < msg.len; i++) {
+      offset += sprintf(buffer+offset, "0x%02x ", msg.data[i]);
     }
-    ThisThread::sleep_for(50ms);
+
+    auto now = timer.elapsed_time();
+    printf("_%llums_ (%llums since previous) CAN message received: [CANID = %d]: %s\r\n",
+        std::chrono::duration_cast<std::chrono::milliseconds>(now).count(),
+        std::chrono::duration_cast<std::chrono::milliseconds>(now - previousTime).count(),
+        msg.id, buffer
+      );
+    
+    previousTime = now;
+    activity = true;
   }
 }
 
 int main() {
+  previousTime = timer.elapsed_time();
+
   ThisThread::sleep_for(5s);
 
   printf(".______    __    __       ___           _______.                             \r\n");
@@ -63,11 +66,10 @@ int main() {
 
   timer.start();
 
-  listenerThread.start(can_listener);
+  canBus.frequency(100*1000);
 
   while(true) {
-    ThisThread::sleep_for(1s);
-    alive = !alive;
+    can_receive();
   }
 
   return 0;
